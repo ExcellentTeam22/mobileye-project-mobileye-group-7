a8621a19365a7f87f808d7c835816282806c91ea
AbedJoulany <abedallah99@gmail.com>
2022-08-10 09:51:59 +0300

    adding attention_crop_result adding labeling adding code of dori

--- crop_images.py
+++ crop_images.py
@@ -1,35 +1,54 @@
 from PIL import Image
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def crop_images():
 
-    #dir = os.getcwd() + "\\" + "train"
-    dir = os.getcwd()+"\\"+ 'leftImg8bit_trainvaltest\\leftImg8bit\\train'
-    #print(os.getcwd())
+    dir = "ME_code/train"
 
-    filename = "attention_results.h5"
+    filename = "ME_code/train_demo/attention_results/attention_results.h5"
     df = pd.read_hdf(filename)
-
+    counter = 0
     list_for_pandas = []
+
+    a = df.iterrows()
+
+    path = next(a)
+
+    row_path = path[1].tolist()[0]
+    full_dir_path = dir + "\\" + row_path.split("_")[0]
+    full_image_path = full_dir_path + "\\" + row_path
+    image = Image.open(full_image_path)
+    counter = 0
+
     for index, row in df.iterrows():
-        full_dir_path = dir + "\\" + (row['path'].split("_"))[0]
-        image = Image.open(full_dir_path + "\\" + row['path'])
+        if row['path'] != row_path:
+            counter = 0
+            row_path = row['path']
+            full_dir_path = dir + "\\" + (row['path'].split("_"))[0]
+            full_image_path = full_dir_path + "\\" + row_path
+            image = Image.open(full_image_path)
+
         if row['col'] == 'r':
-            # img2 = image.crop((row['x'] - 12, row['y'] - 12, row['x'] + 12, row['y'] + 40))
-            list_for_pandas.append((row['path'], row['x'] - 12, row['y'] - 12, row['x'] + 12, row['y'] + 40, row['col']))
-        else:
-            # img2 = image.crop((row['x'] - 20, row['y'] - 115, row['x'] + 25, row['y'] + 20))
-            list_for_pandas.append((row['path'], row['x'] - 20, row['y'] - 115, row['x'] + 25, row['y'] + 20, row['col']))
+            img2 = image.crop((row['x'] - 35*(1 - row['zoom']), row['y'] - 20*(1 - row['zoom']), row['x'] + 35*(1 - row['zoom']), row['y'] + 110*(1 - row['zoom'])))
+            img2 = img2.resize((55, 105))
+            img2.save("train_demo\\crops\\" + str(counter) + row['path'])
+            counter += 1
+            list_for_pandas.append((full_image_path,str(counter) + row['path'], row['x'] - 35*(1 - row['zoom']), row['y'] - 20*(1 - row['zoom']), row['x'] + 35*(1 - row['zoom']), row['y'] + 110*(1 - row['zoom']), row['col']))
 
-    df1 = pd.DataFrame(list_for_pandas, columns=['path', 'x0', 'y0', 'x1', 'y1', 'color'])
-    df1.to_hdf ('attention__crop_results.h5',key='df', mode='w')
+        elif row['col'] == 'g':
+            img2 = image.crop((row['x'] - 35*(1 - row['zoom']), row['y'] - 110*(1 - row['zoom']), row['x'] + 35*(1 - row['zoom']), row['y'] + 20*(1 - row['zoom'])))
+            img2 = img2.resize((55, 105))
+            img2.save("train_demo\\crops\\" + str(counter) + row['path'])
+            counter += 1
+            list_for_pandas.append((full_image_path,str(counter) + row['path'], row['x'] - 35*(1 - row['zoom']), row['y'] - 110*(1 - row['zoom']), row['x'] + 35*(1 - row['zoom']), row['y'] + 20*(1 - row['zoom']), row['col']))
 
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified als
-        print(df1)
+    df1 = pd.DataFrame(list_for_pandas, columns=['fullPath','path', 'x0', 'y0', 'x1', 'y1', 'color'])
 
-
+    df1.to_hdf('crop_results.h5','data')
 
 
 
